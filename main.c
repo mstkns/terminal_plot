e92ccce15ea30e0e0b7e2ca5c60418880a3a6386
mstkns <mar98kus@gmail.com>
2025-06-13 23:01:51 +0200

    Added clear_terminal, set_terminal_size and fill_canvas

--- main.c
+++ main.c
@@ -1,5 +1,40 @@
+#include <sys/ioctl.h>
 #include <stdio.h>
+#include <unistd.h>
+
+int ROWS = 0;
+int COLS = 0;
+
+void clear_terminal() {
+   printf ("\033[H\033[J");
+}
+
+void set_terminal_size() {
+   struct winsize w;
+   ioctl (STDOUT_FILENO, TIOCGWINSZ, &w);
+   ROWS = w.ws_row;
+   COLS = w.ws_col;
+}
+
+void fill_canvas (char c) {
+   for (int i = 0; i < ROWS; i++) {
+      for (int j = 0; j < COLS; j++) {
+         putchar (c);
+      }
+   }
+   putchar ('\n');
+}
 
 int main () {
-   printf ("Hello, World!\n");
+
+   while (1) {
+      clear_terminal();
+      set_terminal_size();
+      fill_canvas ('.');
+      usleep (100000);
+      clear_terminal();
+      set_terminal_size();
+      fill_canvas ('@');
+      usleep (100000);
+   }
 }
